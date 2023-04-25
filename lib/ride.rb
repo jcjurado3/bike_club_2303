b5aaf684dfee1f08a96354c58cb89f8a7dbc45bc
Jimmy Jurado <jurado140@gmail.com>
2023-04-25 11:17:25 -0400

    Initialized rider object with attributes and spec test. Adds total distance method and loop? method.

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,23 @@
 class Ride
+  attr_reader :name, :distance, :loop, :terrain
+
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @loop = info[:loop]
+    @terrain = info[:terrain]
+  end
+
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    if @loop == false
+    @distance * 2
+    else
+      @distance
+    end
+  end
 
 end
---
+++ spec/ride_spec.rb
@@ -0,0 +1,43 @@
+require './lib/ride'
+
+RSpec.describe Ride do
+  describe '#initialize' do
+    it 'exits with attributes' do
+      ride1 = Ride.new(
+        {
+        name: "Walnut Creek Trail", 
+        distance: 10.7, 
+        loop: false, 
+        terrain: :hills})
+
+        expect(ride1).to be_a(Ride)
+        expect(ride1.name).to eq("Walnut Creek Trail")
+        expect(ride1.distance).to eq(10.7)
+        expect(ride1.loop?).to eq(false)
+        expect(ride1.terrain).to eq(:hills)
+    end
+
+    it 'can calculate total distance' do
+      ride1 = Ride.new(
+        {
+        name: "Walnut Creek Trail", 
+        distance: 10.7, 
+        loop: false, 
+        terrain: :hills})     
+
+      expect(ride1.total_distance).to eq(21.4)
+    end
+
+    it 'can calculate total distance when loop is true' do
+      ride2 = Ride.new(
+        {
+          name: "Town Lake", 
+          distance: 14.9, 
+          loop: true, 
+          terrain: :gravel})
+
+      expect(ride2.loop?).to eq(true)
+      expect(ride2.total_distance).to eq(14.9)
+    end
+  end
+end
