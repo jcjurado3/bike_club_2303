86deabd02f18f8f9165040b206191e86ffee7f42
Jimmy Jurado <jurado140@gmail.com>
2023-04-25 13:31:30 -0400

    Adds the best time method and spec test to go along with it.

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -15,4 +15,12 @@ class BikeClub
       biker.rides.values.flatten.count
     end
   end
+
+  def best_time(ride)
+    @bikers.select do |biker| 
+      biker.rides.key?(ride)
+    end.min_by do |biker| 
+      biker.personal_record(ride) 
+    end
+  end
 end
--- spec/bike_club.rb
+++ spec/bike_club.rb
@@ -69,4 +69,35 @@ RSpec.describe BikeClub do
       expect(@bikeclub.most_rides).to eq(biker1)
     end
   end
+
+  describe '#best_time' do  
+    it 'returns the Biker with the best time' do
+      biker1 = Biker.new("Kenny", 30)
+      biker2 = Biker.new("Athena", 15)
+      biker3 = Biker.new("Dakota", 25)
+      biker1.learn_terrain!(:hills)
+      biker2.learn_terrain!(:hills)
+      biker3.learn_terrain!(:hills)
+
+      ride1 = Ride.new(
+        {
+        name: "Walnut Creek Trail", 
+        distance: 10.7, 
+        loop: false, 
+        terrain: :hills})
+
+
+      biker1.log_ride(ride1, 92.5)
+      biker2.log_ride(ride1, 91.1)
+      biker3.log_ride(ride1, 60.9)
+
+      @bikeclub.add_biker(biker1)
+      @bikeclub.add_biker(biker2)
+      @bikeclub.add_biker(biker3)
+
+
+      expect(@bikeclub.best_time(ride1)).to eq(biker3)
+    end
+
+  end
 end
