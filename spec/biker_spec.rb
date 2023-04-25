1a79223f4481bbcdd0053110eb5203ea5ac1cd8c
Jimmy Jurado <jurado140@gmail.com>
2023-04-25 13:48:41 -0400

    Refactor spec test and use of the before block.

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -2,40 +2,34 @@ require './lib/ride'
 require './lib/biker'
 
 RSpec.describe Biker do
+  before(:each) do
+    @biker = Biker.new("Kenny", 30)
+  end
+
   describe '#initialize' do
     it 'exits with attributes' do
-      biker = Biker.new("Kenny", 30)
-
-      expect(biker).to be_a(Biker)
-      expect(biker.name).to eq("Kenny")
-      expect(biker.max_distance).to eq(30)
+      expect(@biker).to be_a(Biker)
+      expect(@biker.name).to eq("Kenny")
+      expect(@biker.max_distance).to eq(30)
     end
 
     it 'has rides empty hash attribute' do
-      biker = Biker.new("Kenny", 30)
-
-      expect(biker.rides).to eq({})
+      expect(@biker.rides).to eq({})
     end
 
     it 'has acceptable terrain array' do
-      biker = Biker.new("Kenny", 30)
-
-      expect(biker.acceptable_terrain).to eq([])
+      expect(@biker.acceptable_terrain).to eq([])
     end
 
     it 'can add new terrain to learn' do
-      biker = Biker.new("Kenny", 30)
-
-      biker.learn_terrain!(:gravel)
-      biker.learn_terrain!(:hills)
-
-      expect(biker.acceptable_terrain).to eq([:gravel, :hills])
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+      expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
     end
 
     it 'can log rides and update rides hash' do
-      biker = Biker.new("Kenny", 30)
-      biker.learn_terrain!(:gravel)
-      biker.learn_terrain!(:hills)
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
       ride1 = Ride.new(
         {
         name: "Walnut Creek Trail", 
@@ -50,23 +44,22 @@ RSpec.describe Biker do
             loop: true, 
             terrain: :gravel}) 
 
-      biker.log_ride(ride1, 92.5)
-      biker.log_ride(ride1, 91.1)
-      biker.log_ride(ride2, 60.9)
-      biker.log_ride(ride2, 61.6)
+      @biker.log_ride(ride1, 92.5)
+      @biker.log_ride(ride1, 91.1)
+      @biker.log_ride(ride2, 60.9)
+      @biker.log_ride(ride2, 61.6)
 
       expected = {
         ride1 => [92.5, 91.1],
         ride2 => [60.9, 61.6]
       }
 
-      expect(biker.rides).to eq(expected)
+      expect(@biker.rides).to eq(expected)
     end
 
     it 'can report personal record for a specific ride' do
-      biker = Biker.new("Kenny", 30)
-      biker.learn_terrain!(:gravel)
-      biker.learn_terrain!(:hills)
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
       ride1 = Ride.new(
         {
         name: "Walnut Creek Trail", 
@@ -81,13 +74,13 @@ RSpec.describe Biker do
             loop: true, 
             terrain: :gravel})
 
-      biker.log_ride(ride1, 92.5)
-      biker.log_ride(ride1, 91.1)
-      biker.log_ride(ride2, 60.9)
-      biker.log_ride(ride2, 61.6)
+      @biker.log_ride(ride1, 92.5)
+      @biker.log_ride(ride1, 91.1)
+      @biker.log_ride(ride2, 60.9)
+      @biker.log_ride(ride2, 61.6)
 
-      expect(biker.personal_record(ride1)).to eq(91.1)
-      expect(biker.personal_record(ride2)).to eq(60.9)
+      expect(@biker.personal_record(ride1)).to eq(91.1)
+      expect(@biker.personal_record(ride2)).to eq(60.9)
     end
 
     it 'testing all methods with conditions' do
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -2,30 +2,23 @@ require './lib/ride'
 
 RSpec.describe Ride do
   describe '#initialize' do
+  before(:each) do
+    @ride1 = Ride.new({
+      name: "Walnut Creek Trail", 
+      distance: 10.7, 
+      loop: false, 
+      terrain: :hills})
+  end
     it 'exits with attributes' do
-      ride1 = Ride.new(
-        {
-        name: "Walnut Creek Trail", 
-        distance: 10.7, 
-        loop: false, 
-        terrain: :hills})
-
-        expect(ride1).to be_a(Ride)
-        expect(ride1.name).to eq("Walnut Creek Trail")
-        expect(ride1.distance).to eq(10.7)
-        expect(ride1.loop?).to eq(false)
-        expect(ride1.terrain).to eq(:hills)
+        expect(@ride1).to be_a(Ride)
+        expect(@ride1.name).to eq("Walnut Creek Trail")
+        expect(@ride1.distance).to eq(10.7)
+        expect(@ride1.loop?).to eq(false)
+        expect(@ride1.terrain).to eq(:hills)
     end
 
-    it 'can calculate total distance' do
-      ride1 = Ride.new(
-        {
-        name: "Walnut Creek Trail", 
-        distance: 10.7, 
-        loop: false, 
-        terrain: :hills})     
-
-      expect(ride1.total_distance).to eq(21.4)
+    it 'can calculate total distance' do     
+      expect(@ride1.total_distance).to eq(21.4)
     end
 
     it 'can calculate total distance when loop is true' do
